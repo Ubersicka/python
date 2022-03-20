11597531f155b683ab17436e342f6cb2b59ec6cf
Metodi Angelov <101995681+Ubersicka@users.noreply.github.com>
2022-03-20 23:04:39 +0200

    Create demo.py
    demo python file

---
+++ demo.py
@@ -0,0 +1,18 @@
+int_number = 10
+float_number = 3.14
+name = "Metodi"
+number = 3.14
+
+
+print(number)
+---------------------------------
+
+name = input()
+
+print(name)
+---------------------------------
+import math
+
+num = math.pi
+
+print(num)
